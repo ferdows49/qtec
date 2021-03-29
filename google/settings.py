54f34d69998ad06e6d0d50432d4535f4a178fb91
Ferdows Ohid <ferdows.ohid49@gmail.com>
2021-03-29 12:26:58 +0600

    edit

--- google/settings.py
+++ google/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')gvzll523z3xxed1a@j1xprbz@r)ydn!*hx=k-+rqe=w6v!fp='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
